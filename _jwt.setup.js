7b4277812013b608dfda5d5b8fa4ab3e69a30079
nhwnahid007 <nhwnahid@gmail.com>
2024-05-08 00:11:08 +0600

    added token

---
+++ _jwt.setup.js
@@ -0,0 +1,21 @@
+/**
+ * #install  jsonweb token
+ * jwt.sign(payload,secret,{expiresIN:})
+ * token client
+ */
+
+
+/**
+ * #How to store token in the client side 
+ * 
+ * 1. memory --> ok type
+ * 2. local storage --> ok type(XSS)
+ * 3. Cookies: http only
+ */
+
+
+/**
+ * 1. set cookies with http only. for development secure: false
+ * 2. cors s
+ * 3. client side axios setting
+ */
